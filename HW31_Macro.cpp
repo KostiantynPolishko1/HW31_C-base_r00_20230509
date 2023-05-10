52a8d22e04c536aab11a6c79350f3503fbceb723
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-05-10 22:34:32 +0300

    4 Added function "checkV()" for check entered value

--- HW31_Macro.cpp
+++ HW31_Macro.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdint>
 using namespace std;
 
 //Write a macro program that takes a numberand converts it into binary code
@@ -15,14 +16,51 @@ while(a) { \
 return x;\
 )
 
+
+int checkV()
+{
+	int a;
+	while (true) // the cycle continues until the user enters the correct value
+	{
+		cin >> a;
+		if (cin.fail()) // if the previous extraction was unsuccessful,
+		{
+			cout << "\tIncorrect input. \n Enter int value: ";
+			cin.clear(); // then return the cin to 'normal' mode of operation
+			cin.ignore(32767, '\n'); // and remove the previous input values from the input buffer
+		}
+		else // if all is well, return a
+		{
+			cin.ignore(32767, '\n'); // and remove the previous input values from the input buffer
+			return abs(a);
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
-	int a = 0;
+	int value = 0, binary = 0;
 
-	cout << "\n Enter value: ";
-	cin >> a;
+	cout << "\n    Enter int value: ";
+	value = checkV();
 
-	cout << BINARY(a) << endl;
+	if (value == 0)
+	{
+		binary = 0;
+		goto end;
+	}
 
+	if (value == 1)
+	{
+		binary = 0;
+		goto end;
+	}
+
+	//binary = BINARY(value);
+end:
+	cout << "\n\tBinary code: " << binary << endl;
+	
 	return 0;
 }
