f7cfd65dda94b7970783adebe2b721cf44f9fbb7
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-05-10 21:20:08 +0300

    2 Updated and shorted logic to convert decimal number into binary code

--- HW31_Macro.cpp
+++ HW31_Macro.cpp
@@ -3,38 +3,32 @@ using namespace std;
 
 //Write a macro program that takes a numberand converts it into binary code
 
+//#define N(b, count) (while (b){ \
+//(b /= 2); \
+//count++; } \
+//return count)
+
 int main()
 {
-	int a = 0, b = a, x = 0;
+	int a = 0, x = 0, temp = 0, d = 1;
 	int count = 0, size = 0;
 
 	cout << "\n Enter value: ";
 	cin >> a;
 
-	while (b)
+	while (a)
 	{
-		b /= 2;
-		count++;
-	}
-	size = count;
+		temp = a % 2;
+		temp *= d;
+		x += temp;
 
-	int* arr = new int[count];
-
-	while (size)
-	{
-		size--;
-		arr[size] = a % 2;
 		a /= 2;
+		/*if(a) 
+			x *= d;*/
+		d *= 10;
 	}
 
-	for (int i = 0; i < count; i++)
-	{
-		x += arr[i];
-		if(i < count-1) x *= 10;
-	}
 	cout << x << endl;
-
-	delete[] arr;
 
 	return 0;
 }
